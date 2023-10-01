ce034af67521d05cf6ad410bdd97b5ee82a56ca6
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-10-01 14:16:20 +0200

    test database migrations

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"go_api/database"
-	server "go_api/handlers"
-	"log"
+    "fmt"
+    "go_api/database"
+    server "go_api/handlers"
+    "log"
 )
 
 func main() {
+    // Run database migrations
+    if err := migrateDatabase(); err != nil {
+        log.Fatal(err)
+    }
 
-	store, err := database.NewPostgresDbConnection()
-	if err != nil {
-		log.Fatal(err)
-	}
+    store, err := database.NewPostgresDbConnection()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Printf("%+v\n", store)
+    fmt.Printf("%+v\n", store)
 
-	server := server.NewAPIServer(":3000", store)
-	server.Run()
+    server := server.NewAPIServer(":3000", store)
+    server.Run()
+}
+
+func migrateDatabase() error {
+    store, err := database.NewPostgresDbConnection()
+    if err != nil {
+        return err
+    }
+
+    defer store.CloseDB()
+
+    if err := store.migrate.Up(); err != nil && err != migrate.ErrNoChange {
+        return err
+    }
+
+    fmt.Println("Database migrations completed.")
+    return nil
 }
