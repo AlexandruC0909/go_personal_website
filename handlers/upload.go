e1b03a2aa93abec7ff7684d02bd450cf3e1e4edb
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-10-03 21:39:52 +0200

    refactor router and handlers to use chi router

--- handlers/upload.go
+++ handlers/upload.go
@@ -13,19 +13,22 @@ type UploadHandler interface {
 	UploadImages(w http.ResponseWriter, r *http.Request) error
 }
 
-func (s *ApiRouter) UploadImages(w http.ResponseWriter, r *http.Request) error {
+func (s *ApiRouter) UploadImages(w http.ResponseWriter, r *http.Request) {
 
 	id, err := getID(r)
 	if err != nil {
-		return err
+		s.handleError(w, r, err)
+		return
 	}
 
 	user, err := s.store.GetUser(id)
 	if err != nil {
-		return err
+		s.handleError(w, r, err)
+		return
 	}
 	if err := r.ParseMultipartForm(32 * 1024 * 1024); err != nil {
-		return err
+		s.handleError(w, r, err)
+		return
 	}
 
 	files := r.MultipartForm.File["file"]
@@ -33,40 +36,47 @@ func (s *ApiRouter) UploadImages(w http.ResponseWriter, r *http.Request) error {
 	for _, fileHeader := range files {
 		file, err := fileHeader.Open()
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 		defer file.Close()
 
 		buff := make([]byte, 512)
 		_, err = file.Read(buff)
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 
 		filetype := http.DetectContentType(buff)
 		if filetype != "image/jpeg" && filetype != "image/png" && filetype != "image/jpg" {
-			return fmt.Errorf("The provided file format is not allowed. Please upload a JPEG, JPG, or PNG image")
+			s.handleError(w, r, err)
+			return
 		}
 
 		err = os.MkdirAll("./static/uploads", os.ModePerm)
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 
 		_, err = file.Seek(0, io.SeekStart)
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 
 		newFileName := fmt.Sprintf("./static/uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename))
 		f, err := os.Create(newFileName)
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 		if len(user.ImageURL) > 2 {
 			err := os.Remove(user.ImageURL[1:])
 			if err != nil {
-				return err
+				s.handleError(w, r, err)
+				return
 			}
 		}
 		user.ImageURL = "." + newFileName
@@ -75,18 +85,19 @@ func (s *ApiRouter) UploadImages(w http.ResponseWriter, r *http.Request) error {
 		defer f.Close()
 		user, err := s.store.GetUser(id)
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 		_, err = io.Copy(f, file)
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 		w.Header().Set("Content-Type", "text/plain")
 		_, err = w.Write([]byte(user.ImageURL))
 		if err != nil {
-			return err
+			s.handleError(w, r, err)
+			return
 		}
 	}
-
-	return nil
 }
