9f1492b88e3869d5c429b9c0dc06ee2191255b8d
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-10-04 19:42:14 +0200

    test embedded templates

--- handlers/posts.go
+++ handlers/posts.go
@@ -1,10 +1,10 @@
 package handlers
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
-	"os"
+
+	templates "go_api/templates"
 )
 
 type PostsHandler interface {
@@ -12,25 +12,12 @@ type PostsHandler interface {
 }
 
 func (s *ApiRouter) handleGetPosts(w http.ResponseWriter, r *http.Request) {
-	templatesDir := os.Getenv("TEMPLATES_DIR")
-	if templatesDir == "" {
-		fmt.Println("TEMPLATES_DIR environment variable is not set.")
-	}
-
-	tmplPathBase := fmt.Sprintf("%s/ui/base.html", templatesDir)
-	tmplPathNav := fmt.Sprintf("%s/ui/navbar.html", templatesDir)
-	tmplPathContent := fmt.Sprintf("%s/posts/postsList.html", templatesDir)
-
-	files := []string{
-		tmplPathBase,
-		tmplPathNav,
-		tmplPathContent,
-	}
-	tmpl, err := template.ParseFiles(files...)
+	tmpl, err := template.ParseFS(templates.Templates, "ui/base.html", "ui/navbar.html", "posts/postsList.html")
 	if err != nil {
 		s.handleError(w, r, err)
 		return
 	}
+
 	err = tmpl.Execute(w, nil)
 	if err != nil {
 		s.handleError(w, r, err)
