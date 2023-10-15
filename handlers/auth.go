140723a9cd4625d9731c7448499cbf35323a4269
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-10-15 22:24:26 +0200

    add error message to register

--- handlers/auth.go
+++ handlers/auth.go
@@ -95,9 +95,33 @@ func (s *ApiRouter) handleRegisterPost(w http.ResponseWriter, r *http.Request) {
 		s.handleError(w, r, err)
 		return
 	}
+	if createAccReq.Password != createAccReq.ConfirmPassword {
+		tmpl, err := template.ParseFS(templates.Templates, "ui/registerError.html")
+		if err != nil {
+			s.handleError(w, r, err)
+			return
+		}
+		errorMessage := "Passwords don't match."
+		err = tmpl.Execute(w, errorMessage)
+		if err != nil {
+			s.handleError(w, r, err)
+			return
+		}
+		return
+	}
 	existingUser, _ := s.store.GetUserByEmail(createAccReq.Email)
 	if existingUser != nil {
-		WriteJSON(w, http.StatusForbidden, ApiError{Error: createAccReq.Email + " already used"})
+		tmpl, err := template.ParseFS(templates.Templates, "ui/registerError.html")
+		if err != nil {
+			s.handleError(w, r, err)
+			return
+		}
+		errorMessage := "Email Already Exists."
+		err = tmpl.Execute(w, errorMessage)
+		if err != nil {
+			s.handleError(w, r, err)
+			return
+		}
 		return
 	}
 	user, err := types.NewUser(createAccReq.FirstName, createAccReq.LastName, createAccReq.Email, createAccReq.Password)
--- types/auth.go
+++ types/auth.go
@@ -5,10 +5,11 @@ import (
 )
 
 type RegisterRequest struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirmPassword"`
 }
 
 type LoginResponse struct {
